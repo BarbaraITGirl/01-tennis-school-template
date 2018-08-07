80c5a906d0e31e9ce4a218e55aa39ca587f4be0a
BarbaraTackaBa <41183147+BarbaraTackaBa@users.noreply.github.com>
2018-08-07 23:09:47 +0200

    #1Template
    HTML, CSS (FlexBox Grid CSS), SASS (Koala Compiler), JS, AOS (Plugin)

---
+++ js/index.js
@@ -0,0 +1,28 @@
+/*Video background*/
+function ShowMsg() {
+    document.getElementById('msg_box').style.display = 'block';
+}
+function CloseMsg(){
+    document.getElementById('msg_box').style.display = 'none';
+}
+
+/*Courses*/
+function ShowInfo() {
+    document.getElementById('msg_info').style.display = 'block';
+}
+function CloseInfo(){
+    document.getElementById('msg_info').style.display = 'none';
+}
+function ShowInfo1() {
+    document.getElementById('msg_info1').style.display = 'block';
+}
+function CloseInfo1(){
+    document.getElementById('msg_info1').style.display = 'none';
+}
+function ShowInfo2() {
+    document.getElementById('msg_info2').style.display = 'block';
+}
+function CloseInfo2(){
+    document.getElementById('msg_info2').style.display = 'none';
+}
+
